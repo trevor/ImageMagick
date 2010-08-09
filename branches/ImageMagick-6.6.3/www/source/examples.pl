6f2522b0bf84c8807b7ea354f9f9e5f30f81a9ab
cristy <cristy@aa41f4f7-0bf4-0310-aa73-e5a19afd5a74>
2010-08-09 02:24:10 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick@2399 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- branches/ImageMagick-6.6.3/www/source/examples.pl
+++
@@ -1,2 +0,0 @@
-Error: Can't locate object method "Label" via package "Exception 410: no images defined `Image::Magick' @ error/Magick.xs/XS_Image__Magick_Clone/2886" (perhaps you forgot to load "Exception 410: no images defined `Image::Magick' @ error/Magick.xs/XS_Image__Magick_Clone/2886"?) at /var/www/html/ImageMagick/source/examples.pl line 35.
-
